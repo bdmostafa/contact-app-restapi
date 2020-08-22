6bd48405d55fe9809c15d73efd547bec48321080
Mostafa <mdmostafaalmahmud@gmail.com>
2020-08-22 11:08:57 +0600

    edit/update functionality

--- src/UI.js
+++ src/UI.js
@@ -5,9 +5,14 @@ class UI {
         this.lastName = document.getElementById('lastName');
         this.email = document.getElementById('email');
         this.phone = document.getElementById('phone');
+        this.submitBtn = document.getElementById('submit');
+        this.parentSubmitBtn = document.getElementById('parent-submit-btn');
+        this.idInput = document.getElementById('id');
+
     }
     showContacts(contacts) {
         let output = '';
+        // console.log(contacts);
         contacts.forEach(contact => {
             // Object destructuring
             const { firstName, lastName, email, phone } = contact;
@@ -22,6 +27,37 @@ class UI {
                     `
             this.contactDisplay.innerHTML = output;
         })
+    }
+    fillForm({ firstName, lastName, email, phone, id }) {
+        this.firstName.value = firstName;
+        this.lastName.value = lastName;
+        this.email.value = email;
+        this.phone.value = phone;
+        this.idInput.value = id;
+    }
+    handleBtnEdit() {
+        // Change submit button to update button
+        this.submitBtn.textContent = 'Update';
+        this.submitBtn.classList.remove('btn-primary');
+        this.submitBtn.classList.add('btn-info');
+        // Create Back button
+        const backBtn = document.createElement('button');
+        backBtn.className = 'btn btn-danger w-100 mb-1';
+        backBtn.id = 'back-btn';
+        backBtn.textContent = 'Back';
+        // Insert backBtn
+        this.parentSubmitBtn.insertBefore(backBtn, this.submitBtn);
+    }
+    changeState(state) {
+        if (state === 'add') {
+            if (document.getElementById('back-btn')) {
+                document.getElementById('back-btn').remove();
+            }
+            this.submitBtn.textContent = 'Submit';
+            this.submitBtn.classList.remove('btn-info');
+            this.submitBtn.classList.add('btn-primary');
+            this.idInput.value = '';
+        }
     }
     clearFields() {
         this.firstName.value = '';
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
+// Importing http and ui
 import { http } from './http';
 import { ui } from './ui';
 
+// Selectors and addEventListeners =============================================
 // Getting contracts when DOM loaded
 document.addEventListener('DOMContentLoaded', getContacts);
 // Handle form submit
 document.getElementById('form').addEventListener('submit', submitContact);
 // Delete contact
 document.getElementById('contact-list').addEventListener('click', deleteContact);
+// Update contact
+document.getElementById('contact-list').addEventListener('click', editContact);
+// Handle back button by event delegation
+document.getElementById('form').addEventListener('click', backFromUpdate);
 
+// Functions ======================================
 function getContacts() {
     http
         .get('http://localhost:3000/contacts')
@@ -18,11 +25,13 @@ function getContacts() {
 
 function submitContact(e) {
     e.preventDefault();
+    console.log('submit')
     // Selectors
     const firstName = document.getElementById('firstName').value;
     const lastName = document.getElementById('lastName').value;
     const email = document.getElementById('email').value;
     const phone = document.getElementById('phone').value;
+    const id = document.getElementById('id').value;
 
     // Validation
     if (firstName === '' || lastName === '' || email === '' || phone === '') {
@@ -35,29 +44,69 @@ function submitContact(e) {
             email,
             phone
         }
-        // Applying POST method under http.js
-        http
-            .post('http://localhost:3000/contacts', data)
-            .then(() => {
-                // console.log(data);
-                ui.clearFields();
-                getContacts();
-            })
-
+        // Checking state of form whether id is empty string in user's add state or not
+        if (id === '') {
+            // Applying POST method under http.js
+            http
+                .post('http://localhost:3000/contacts', data)
+                .then(() => {
+                    // console.log(data);
+                    ui.clearFields();
+                    getContacts();
+                })
+        } else {
+            console.log('ok')
+            // If id is not empty string, Update contacts in the server
+            http
+                .update(`http://localhost:3000/contacts/${id}`, data)
+                .then(() => {
+                    ui.clearFields();
+                    // Changing state of the selected contact
+                    ui.changeState('add');
+                    // Display updated contact information on UI
+                    getContacts();
+                })
+        }
     }
 }
 
 function deleteContact(e) {
     // console.log(e.target);
     const id = e.target.dataset.id;
-    console.log(id);
+    // console.log(id);
     if (e.target.classList.contains('fa-trash')) {
         http
             .delete(`http://localhost:3000/contacts/${id}`)
             .then(() => {
-                console.log('contact deleted.');
+                // console.log('contact deleted');
                 getContacts();
             })
             .catch(err => console.log(err));
+    }
+}
+
+function editContact(e) {
+    const id = e.target.dataset.id;
+    // Checking edit icon of target event
+    if (e.target.classList.contains('fa-edit')) {
+        // Request for getting the selected data
+        http
+            .get(`http://localhost:3000/contacts/${id}`)
+            .then(data => {
+                // console.log(data);
+                ui.fillForm(data);
+                ui.handleBtnEdit();
+            })
+    }
+}
+
+function backFromUpdate(e) {
+    // console.log(e.target.id);
+    e.preventDefault();
+
+    if (e.target.id === 'back-btn') {
+        // Changing form to add state
+        ui.changeState('add');
+        ui.clearFields();
     }
 }
